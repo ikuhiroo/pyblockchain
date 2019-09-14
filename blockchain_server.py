e420337a9afd855d6bf5a428f13e53caa229758f
ikuhiroo <i2ku.nn@gmail.com>
2019-09-15 00:54:27 +0900

    blockchain nodeの立ち上げ

--- blockchain_server.py
+++ blockchain_server.py
@@ -6,10 +6,36 @@ import wallet
 
 app = Flask(__name__)
 
+cache = {}
 
-@app.route("/")
-def hello_world():
-    return "Hello, world!"
+
+def get_blockchain():
+    # blockchainの情報は本来DBに入れる
+    # 今回はglobalにchcheに入れる
+    # すぐに呼び出せるようにする
+    cached_blockchain = cache.get("blockchain")
+    # １度しか呼ばれない
+    if not cached_blockchain:
+        miners_wallet = wallet.Wallet()
+        cache["blockchain"] = blockchain.BlockChain(
+            blockchain_address=miners_wallet.blockchain_address,
+            port=app.config["port"]
+        )
+        app.logger.warning({
+            "private_key": miners_wallet.private_key,
+            "public_key": miners_wallet.public_key,
+            "blockchain_address": miners_wallet.blockchain_address
+        })
+    return cache["blockchain"]
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    block_chain = get_blockchain()
+    response = {
+        'chain': block_chain.chain
+    }
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
@@ -22,6 +48,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     port = args.port
+
+    app.config["port"] = port
 
     # 同時リクエストを引き受ける
     app.run(host="0.0.0.0", port=port, threaded=True, debug=True)
