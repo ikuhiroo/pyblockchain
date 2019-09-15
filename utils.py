f3dbece0e8ed9e2b40fbed2bcea69f93f73b4449
ikuhiroo <i2ku.nn@gmail.com>
2019-09-15 12:03:02 +0900

    socket like p2p

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
 import hashlib
 import collections
+import logging
+import re
+import socket
+
+logger = logging.getLogger(__name__)
+
+RE_IP = re.compile(
+    "(?P<prefix_host>^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.)(?P<last_ip>\\d{1,3}$)")
 
 
 def sorted_dict_by_key(unsorted_dict):
@@ -29,12 +37,60 @@ def pprint(chains):
                 for d in v:
                     print(f'{"-"*40}')
                     for kk, vv in d.items():
-                        print(f'{kk:30}{vv}')
+                        print(f"{kk:30}{vv}")
             else:
-                print(f'{k:15}{v}')
+                print(f"{k:15}{v}")
     print(f'{"*"*25}')
+
+
+def is_found_host(target, port):
+    """他のノードが立ち上がっているか調べるsocket\n
+    netword address: AF_INET\n
+    TCP/IP: socket.SOCK_STREAM\n
+    # python blockchain_server.py
+    # >> is_found_host("127.0.0.1", 5000)
+    # True
+    """
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(1)
+        try:
+            sock.connect((target, port))
+            return True
+        except Exception as ex:
+            logger.error({
+                "action": "is_found_host",
+                "target": target,
+                "port": port,
+                "ex": ex
+            })
+            return False
+
+
+def find_neighbours(my_host, my_port, start_ip_range, end_ip_range, start_port, end_port):
+    # 192.168.0.24 (1,3)
+    address = f"{my_host}:{my_port}"
+    m = RE_IP.search(my_host)
+    if not m:
+        return None
+
+    prefix_host = m.group("prefix_host")
+    last_ip = m.group("last_ip")
+
+    neighbours = []
+    for guess_port in range(start_port, end_port):
+        for ip_range in range(start_ip_range, end_ip_range):
+            guess_host = f"{prefix_host}{int(last_ip)+int(ip_range)}"
+            guess_address = f"{guess_host}:{guess_port}"
+            # ignored my_host
+            if is_found_host(guess_host, guess_port) and not guess_address == address:
+                neighbours.append(guess_address)
+    return neighbours
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+
+    # print(is_found_host("127.0.0.1", 5000))
+    print(find_neighbours("192.168.0.21", 5000, 0, 3, 5000, 5003))
